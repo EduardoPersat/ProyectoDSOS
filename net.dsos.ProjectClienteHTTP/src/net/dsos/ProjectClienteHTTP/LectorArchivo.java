9b262ed529251aa106f1b7853ca724c55f589a85
EduardoPersat <14161328@itoaxaca.edu.mx>
2018-05-09 14:32:18 -0500

    Projecto dsos

---
+++ net.dsos.ProjectClienteHTTP/src/net/dsos/ProjectClienteHTTP/LectorArchivo.java
@@ -0,0 +1,21 @@
+package net.dsos.ProjectClienteHTTP;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class LectorArchivo {
+	
+	public List<Integer> parsear(InputStream archivo)throws IOException {
+		
+		List<Integer> numeros = new ArrayList<>();
+		
+		
+		
+		return numeros;
+	}
+
+}
